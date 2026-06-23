a980cde04d861b5043412f61ef5ad02f91dcced1
= <=>
2026-06-23 01:47:51 +0100

    Integrating Header and Footer

--- components/header.js
+++ components/header.js
@@ -1,18 +1,19 @@
 
 export default function Header(){
+    const logoSrc = '/biokinetix/data/logo.png';
 
     return `
     <header>
         <div class="logo">
-            <img src="data/logo.png" alt="" srcset="">
+            <img src="${logoSrc}" alt="BioKinetiX logo" srcset="">
         </div>
         <nav>
-            <a href="index.html">Home</a>
-            <a href="">About</a>
-            <a href="">Team</a>
-            <a href="">Model</a>
-            <a href="">Simulation</a>
-            <a href="">Analysis</a>
+            <a href="/biokinetix/index.html">Home</a>
+            <a href="/biokinetix/about.html">About</a>
+            <a href="/biokinetix/team.html">Team</a>
+            <a href="/biokinetix/mathematical_model/model.html">Model</a>
+            <a href="/biokinetix/simulation.html">Simulation</a>
+            <a href="/biokinetix/analysis.html">Analysis</a>
         </nav>
     </header>
     `;
