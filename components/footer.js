819311d76b3640f025f505197089f0bbfd24d5d3
= <=>
2026-06-23 01:12:29 +0100

    Header and Footer

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+
+export default function Footer(){
+    return `
+    <footer>
+        <p>BioKinetiX Team</p>
+        <p>&copy; Copyright 2026</p>
+    </footer>
+    `;
+}
---
+++ components/header.js
@@ -0,0 +1,19 @@
+
+export default function Header(){
+
+    return `
+    <header>
+        <div class="logo">
+            <img src="data/logo.png" alt="" srcset="">
+        </div>
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="">About</a>
+            <a href="">Team</a>
+            <a href="">Model</a>
+            <a href="">Simulation</a>
+            <a href="">Analysis</a>
+        </nav>
+    </header>
+    `;
+}
